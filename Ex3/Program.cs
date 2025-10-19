64f8261a467d241c3d5ebd9602544da3b75aa5e2
Шапкин Николай <kosmys122@gmail.com>
2025-10-19 16:08:52 +0300

    Задание 3

---
+++ Ex3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Ex3
+{
+    internal class Program
+    {
+        public delegate void Logger(string message);
+        static void Main(string[] args)
+        {
+            void LogToConsole(string message)
+            {
+                Console.WriteLine(message);
+            }
+            Logger print = LogToConsole;
+            print("Задание номер 3 выполнено");
+        }
+    }
+}
