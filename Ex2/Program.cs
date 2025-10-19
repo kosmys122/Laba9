71d7a3f70233ca207978fa84cd98173d2944bad3
Шапкин Николай <kosmys122@gmail.com>
2025-10-19 14:23:40 +0300

    Задание 2

---
+++ Ex2/Program.cs
@@ -0,0 +1,18 @@
+﻿using HtmlAgilityPack;
+namespace Ex2
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            Console.Write("Введите URL сайта: ");
+            string? url = Console.ReadLine();
+            HtmlDocument doc = new();
+            HttpClient client = new();
+            string html = await client.GetStringAsync(url);
+            doc.LoadHtml(html);
+            var titleNode = doc.DocumentNode.SelectSingleNode("//title");
+            Console.WriteLine(titleNode.InnerText);
+        }
+    }
+}
