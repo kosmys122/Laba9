1aecd466d1cd5236b6d9766505b3fd54ffdad8cb
Шапкин Николай <kosmys122@gmail.com>
2025-10-19 16:21:26 +0300

    Задание 4

---
+++ Ex4/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thermometer item = new();
+            item.TemperatureTooHigh += () => Console.WriteLine("Срочно снизте температуру!");
+            item.Measure(10);
+            item.Measure(33);
+            item.Measure(80);
+            item.Measure(1);
+        }
+    }
+}
---
+++ Ex4/Thermometer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4
+{
+    internal class Thermometer
+    {
+        public event Action TemperatureTooHigh;
+        public int temp = 0;
+        public void Measure(int amoynt)
+        {
+            temp += amoynt;
+            if (temp > 100)
+            {
+                MaxTemp();
+            }
+            else
+                Console.WriteLine($"Температура: {temp}");
+        }
+        private void MaxTemp()
+        {
+            Console.WriteLine("Температура превысела ограничение!");
+            TemperatureTooHigh?.Invoke();
+        }
+    }
+}
