acf28200e1231ead8305a19f1ca8018aeccf0800
Шапкин Николай <kosmys122@gmail.com>
2025-10-21 16:40:54 +0300

    Задание 5

---
+++ Ex5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void PrintLength(string? input)
+            {
+                if (input==null)
+                    Console.WriteLine("Строка отсутствует!");
+                else
+                    Console.WriteLine(input.Length);
+            }
+            PrintLength(null);
+            PrintLength("31");
+        }
+    }
+}
