c27f1b95644b7db2c8d5b96e1c1c9cddfac401d9
Шапкин Николай <kosmys122@gmail.com>
2025-10-21 17:05:46 +0300

    Задание 6

---
+++ Ex6/Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+namespace Ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SmartLight lampa = new();
+            lampa.OnStateChanged += OnStateChanged;
+            lampa.Toggle(false, 5);
+            lampa.Toggle(false, 1);
+            string json = JsonConvert.SerializeObject(lampa);
+            Console.WriteLine("В JSON: "+json);
+        }
+        static void OnStateChanged()
+        {
+            Console.WriteLine("Свет включён / Свет выключен");
+        }
+    }
+}
---
+++ Ex6/SmartLight.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex6
+{
+    internal class SmartLight
+    {
+        public event Action OnStateChanged;
+        private Boolean IsOn;
+        private int Brightness;
+        public void Toggle(Boolean flag,int strang)
+        {
+            IsOn = flag;
+            Brightness = strang;
+            OnStateChanged?.Invoke();
+        }
+    }
+}
